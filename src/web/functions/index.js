5fd9dc0a91206301816f01f4f225244a7875e4ce
matthew.rubacky <matthew.rubacky@orasi.com>
2018-04-23 17:53:38 -0400

    Added new page

--- src/web/functions/index.js
+++ src/web/functions/index.js
@@ -21,12 +21,4 @@ exports.removeUser = functions.auth.user().onDelete((user) => {
 	const docRef = admin.firestore().doc('users/'+uid);
 	
 	return docRef.delete();
-}
-
-//exports.addTea = functions.https.onCall((data, context) => ){
-//	var uid = context.auth.uid
-//	var teaname = data.name
-// var teatype = data.type
-// var teatime = data.time
-// var teatemp = data.temp
-//}
+});
---
+++ src/web/public/js/firebaseinit.js
@@ -0,0 +1,9 @@
+	var config = {
+	    		apiKey: "",
+	    		authDomain: "iroh-c159b.firebaseapp.com",
+	    		databaseURL: "https://iroh-c159b.firebaseio.com",
+	    		projectId: "iroh-c159b",
+	    		storageBucket: "iroh-c159b.appspot.com",
+	    		messagingSenderId: "747454468018"
+	  	};
+	  firebase.initializeApp(config);
